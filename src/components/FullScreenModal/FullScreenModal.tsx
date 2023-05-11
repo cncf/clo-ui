fa598223cca3e01cb78ff76e59d347bc6b425fe8
Cintia Sánchez García <cynthiasg@icloud.com>
2023-05-11 18:55:13 +0200

    Improve full screen modal component (#25)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- src/components/FullScreenModal/FullScreenModal.tsx
+++ src/components/FullScreenModal/FullScreenModal.tsx
@@ -1,18 +1,30 @@
 import isUndefined from 'lodash/isUndefined';
-import React, { MouseEvent as ReactMouseEvent, useEffect, useState } from 'react';
+import React, { MouseEvent as ReactMouseEvent, MutableRefObject, useEffect, useRef, useState } from 'react';
 
-import { useBodyScroll } from '../../hooks';
+import { useBodyScroll, useOutsideClick } from '../../hooks';
 import styles from './FullScreenModal.module.css';
 
 export interface IFullScreenModalProps {
   children: JSX.Element | JSX.Element[];
   open?: boolean;
   onClose?: () => void;
+  breakPoint?: string;
+  excludedRefs?: MutableRefObject<HTMLDivElement | null>[];
+  unclosable?: boolean;
 }
 
 export const FullScreenModal = (props: IFullScreenModalProps) => {
   const [openStatus, setOpenStatus] = useState(props.open || false);
-  useBodyScroll(openStatus, 'modal');
+  const button = useRef<HTMLButtonElement>(null);
+  const unclosable = !isUndefined(props.unclosable) && props.unclosable;
+  useOutsideClick(
+    [button, ...(!isUndefined(props.excludedRefs) ? [...props.excludedRefs] : [])],
+    openStatus && !unclosable,
+    () => {
+      closeModal();
+    }
+  );
+  useBodyScroll(openStatus, 'modal', props.breakPoint);
 
   const closeModal = () => {
     setOpenStatus(false);
@@ -46,6 +58,7 @@ export const FullScreenModal = (props: IFullScreenModalProps) => {
     <div className={`position-fixed overflow-hidden p-3 top-0 bottom-0 start-0 end-0 ${styles.modal}`} role="dialog">
       <div className={`position-absolute ${styles.closeWrapper}`}>
         <button
+          ref={button}
           type="button"
           className={`btn-close btn-close-white opacity-100 fs-5 ${styles.close}`}
           onClick={(e: ReactMouseEvent<HTMLButtonElement>) => {
